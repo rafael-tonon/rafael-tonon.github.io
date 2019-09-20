66656d922f57693ba28f0e5100f93e2caebe18ee
Rafael Tonon <rafael.tonon@protonmail.com>
2019-09-20 03:47:52 -0300

    Mobile page redo

--- js/typewriter.js
+++ js/typewriter.js
@@ -1,26 +1,19 @@
 var header = document.getElementById("top");
 window.onload = typeWriter;
 
-
-function randomizer(){
-
-}
-
-
 function typeWriter(){
     let typeW = document.getElementsByClassName("typeW");
     let x=0, y=0;
 
     let name = "Rafael Tonon";
-    let title = "Estudante Front-End";
+    //let title = "Estudante Front-End";
+    let title = "Web Dev";
 
     typeW[0].innerHTML = "";
     typeW[1].innerHTML = "";
 
     let typing = setInterval(e => {
-        
         if (x < name.length){
-            //typeW[0].classList.add("kb-cursor");
             typeW[0].innerHTML += name[x];
             x++;
         
@@ -31,6 +24,8 @@ function typeWriter(){
             y++;
         
         } else {
+            typeW[1].classList.remove("kb-cursor");
+            typeW[1].classList.add("kb-cursor-animation");
             clearInterval(typing);
         }
         
